fd830c56b253f307ecc8b74534cc6f886cb9d0ab
Johannes Kümmel <johannes.kummel@roguewave.com>
2018-07-25 14:27:43 +0300

    Replaced joining paths as string with File built-in path-joining

--- src/test/java/ee/ttu/java/studenttester/tests/Tests.java
+++ src/test/java/ee/ttu/java/studenttester/tests/Tests.java
@@ -225,7 +225,7 @@ public class Tests {
 	 * @return success
 	 */
 	public boolean addFile(final String code, final String name, final boolean isTest) {
-		String arg = String.format(tempDirName + "%s/%s.java", isTest ? "/test" : "/source", name);
+		String arg = new File(tempDirName, String.format("%s/%s.java", isTest ? "test" : "source", name)).getPath();
 		try (PrintWriter writer = new PrintWriter(arg, "UTF-8")) {
 			writer.write(code);
 			File f = new File(arg);
@@ -248,7 +248,7 @@ public class Tests {
 	 * @return test results in JSON format
 	 */
 	public JSONObject getTestResults(final boolean checkStyleEnabled, final boolean testNGEnabled) {
-		StudentTesterMain c = new StudentTesterMain(tempDirName + "/test/", tempDirName + "/source/");
+		StudentTesterMain c = new StudentTesterMain(new File(tempDirName, "test").getPath(), new File(tempDirName, "source").getPath());
 		c.enableCheckstyle(checkStyleEnabled);
 		c.enableTestNG(testNGEnabled);
 		c.outputJSON(true);
@@ -316,9 +316,9 @@ public class Tests {
 		if (tempDirName == null) {
 			throw new NullPointerException("Temp folder not found!");
 		}
-		testDir = new File(tempDirName + "/test");
+		testDir = new File(tempDirName, "test");
 		testDir.mkdir();
-		sourceDir = new File(tempDirName + "/source");
+		sourceDir = new File(tempDirName, "source");
 		sourceDir.mkdir();
 	}
 
